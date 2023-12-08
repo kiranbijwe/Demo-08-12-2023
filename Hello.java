dc79434979550096448110f715f7cee886390d78
kiranBijwe <kiranbijwe12@gmail.com>
2023-12-08 17:49:48 +0530

    feature1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello
-	{
+	{	
+
+		public void feature1()
+		{
+			System.out.prontln("feature1");
+		}
+
 		public static void main(String args[])
 			{
 				System.out.println("Hello Git ");
